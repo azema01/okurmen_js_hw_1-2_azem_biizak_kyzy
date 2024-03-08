066584d5ea4c49cdff9bca6beed132aa59eca385
biizakkyzyazem <azemeshimova@gmail.com>
2024-03-08 17:10:14 +0600

    js 1-2

---
+++ index.js
@@ -0,0 +1,43 @@
+// let total = 15 + 7*2+19;
+// alert (' 15+7*2+19='+ total);
+
+let string =" 48, рекет намаз окулду";
+alert(string[0]
+.toLowerCase ()+ string );
+
+// const string ="эМнеГе аЙгаНыш аСельди АЛДАДЫ";
+// alert (string[0].toUpperCase () + string 
+// [1].toLowerCase()+ string
+// [2].toLowerCase()+ string
+// [3].toLowerCase()+ string
+// [4].toLowerCase()+ string
+// [5].toLowerCase()+ string
+
+// [6].toUpperCase()+ string
+
+// [7].toUpperCase()+ string
+// [8].toLowerCase()+ string
+// [9].toLowerCase()+ string
+// [10].toLowerCase()+ string
+// [11].toLowerCase()+ string
+// [12].toLowerCase()+ string
+// [13].toLowerCase()+ string
+
+// [14].toLowerCase()+ string
+
+// [15].toUpperCase()+ string
+// [16].toLowerCase()+ string
+// [17].toLowerCase()+ string
+// [18].toLowerCase()+ string
+// [19].toLowerCase()+ string
+// [20].toLowerCase()+ string
+// [21].toLowerCase()+ string
+
+// [22].toLowerCase()+ string
+// [23].toLowerCase()+ string
+// [24].toLowerCase()+ string
+// [25].toLowerCase()+ string
+// [26].toLowerCase()+ string
+// [27].toLowerCase()+ string
+
+// [28].toLowerCase()+ string.slice(29).toLowerCase()+'?');
